f15210927c2993892e3ac9aabe6d4b23417319ee
Caio Lins <wolflow429@gmail.com>
2023-05-02 12:40:13 +0900

    add 'no task' ui and build for mac app

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,5 +1,13 @@
 <template>
   <q-page class="bg-grey-3 column">
+    <div class="row q-pa-sm bg-primary">
+      <q-input v-model="newTask" @keyup.enter="addTask" class="col" square filled bg-color="white" placeholder="Add task"
+        dense>
+        <template v-slot:append>
+          <q-btn @click="addTask" round dense flat icon="add" />
+        </template>
+      </q-input>
+    </div>
     <q-list class="bg-white" separator bordered>
       <q-item v-for="(task, index) in tasks" :key="task.title" @click="task.done = !task.done"
         :class="{ 'done bg-blue-1': task.done }" v-ripple clickable>
@@ -14,6 +22,10 @@
         </q-item-section>
       </q-item>
     </q-list>
+    <div v-if="!tasks.length" class="no-tasks absolute-center">
+      <q-icon name="check" size="100px" color="primary" />
+      <div class="text-h5 text-primary text-center">No tasks</div>
+    </div>
   </q-page>
 </template>
 
@@ -21,24 +33,7 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  data() {
-    return {
-      tasks: [
-        {
-          title: 'Buy bananas',
-          done: false
-        },
-        {
-          title: 'Buy apples',
-          done: false
-        },
-        {
-          title: 'Buy grapes',
-          done: false
-        }
-      ]
-    }
-  },
+  data() { return { newTask: '', tasks: [{ title: 'Dummy task', done: false }] } },
   methods: {
     deleteTask(index: number) {
       this.$q.dialog({
@@ -46,7 +41,14 @@ export default defineComponent({
         message: 'Really delete?',
         cancel: true,
         persistent: true
-      }).onOk(() => this.tasks.splice(index, 1))
+      }).onOk(() => {
+        this.tasks.splice(index, 1)
+        this.$q.notify('Task deleted')
+      })
+    },
+    addTask() {
+      this.tasks.push({ title: this.newTask, done: false })
+      this.newTask = ''
     }
   }
 });
